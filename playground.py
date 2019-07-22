2ca50bb912666373063e10938a79aedbcfec7c8d
SOL COVACICH <socovacich@uc.cl>
2019-07-22 16:12:29 -0400

    raws en bw

--- playground.py
+++ playground.py
@@ -1,5 +1,5 @@
 # https://pypi.org/project/pytesseract/
-
+import cv2
 try:
     from PIL import Image
 except ImportError:
@@ -23,18 +23,19 @@ print(pytesseract.image_to_string(Image.open(f'refranes/refranes-002.png'), lang
 print('\n==============\n')
 print(pytesseract.image_to_string(Image.open(f'img/pag2.png'), lang='spa'))
 '''
-for n in range(1,101):
-    with open('raws/pag{:0>3d}.txt'.format(n), 'w') as pag:
-        pag.write(pytesseract.image_to_string(Image.open('refranes/refranes-{:0>3d}.png'.format(n)), lang='spa'))
 
 
-'''
-Esto es una weaita que toy testeando xd
+def read():
+    for n in range(1, 101):
+        with open('raws_bw/pag{:0>3d}.txt'.format(n), 'w') as pag:
+            pag.write(pytesseract.image_to_string(Image.open('refranes_bw/refranes_bw-{:0>3d}.png'.format(n)), lang='spa'))
 
+# print(pytesseract.image_to_string(Image.open('raws_bw/test.png'), lang='spa'))
 
-img = Image.open('testx.png')
-print(pytesseract.image_to_string(img), '\n----\n')
-new_size = tuple(2*x for x in img.size)
-img = img.resize(new_size, Image.ANTIALIAS)
-print(pytesseract.image_to_string(img))
-'''
+def grayscale():
+    for n in range(1,101):
+        image = cv2.imread('refranes/refranes-{:0>3d}.png'.format(n))
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite('refranes_bw/refranes_bw-{:0>3d}.png'.format(n), gray)
+
+read()
