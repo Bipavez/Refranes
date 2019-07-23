e8a2ad420dcc300faf6829da2e31c2d900234846
bipavez <bipo1998@gmail.com>
2019-07-23 09:52:23 -0400

    arreglos
    Queda ver páginas en donde salen objetos solos. Cuando el refrán está en el final de página. Ver otras excepciones y caractéres especiales para incluir en el regex.

--- extraccion.py
+++ extraccion.py
@@ -4,8 +4,8 @@ import os
 
 def pasapalabra(pag):
     #uhm, a esta función pasemosle el texto q sale de pytesseract
-    pattern = "(?<=(\n){2}|.\n)([a-zA-Z,\. áéíóúÁÉÍÓÚñÑ]*)(\n)?(?=\n([a-zA-Z 0-9áéíóúÁÉÍÓÚñÑ]){0,5}((?i)([a-z]){3}(\.|,)( )?(?i)([a-z])(\.|,)( )?(?i)([a-z])(\.|,)|(?i)([a-z]){3}(\.|,)( )?([0-9]){3}|([a-zA-Z]){2}\.:))"
-    prelista = (item[1] for item in re.findall(pattern, pag))
+    pattern = "(?<=(\n){2}|.\n|^..)([a-zA-Z,\. áéíóúÁÉÍÓÚñÑ\(\)]*)(\n)?(?=\n([a-zA-Z 0-9áéíóúÁÉÍÓÚñÑ]){0,5}((?i)([a-z]){3}(\.|,)( )?(?i)([a-z])(\.|,)( )?(?i)([a-z])(\.|,)|(?i)([a-z]){3}(\.|,)( )?([0-9]){3}|([a-zA-Z]){2}\.:))"
+    prelista = (item[1].rstrip("\n") for item in re.findall(pattern, pag))
     resultado = filter(lambda x: x, prelista)
     return resultado
 if __name__ == '__main__':
